91add06e7c9e7b1299c317d536736b975f9c3a87
Kim Hee ju <jju95@gmail.com>
2022-10-13 01:38:14 +0900

    httpApiToJson

---
+++ src/main/java/hello/servlet/basic/HelloData.java
@@ -0,0 +1,15 @@
+package hello.servlet.basic;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter
+public class HelloData {
+    // DTO
+
+    private String username;
+    private int age;
+}
+
