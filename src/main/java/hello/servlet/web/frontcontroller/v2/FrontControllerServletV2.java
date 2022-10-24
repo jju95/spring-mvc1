e05957ec1f679e37ab58916f83dd4b4b50f28740
Kim Hee ju <jju95@gmail.com>
2022-10-24 20:23:30 +0900

    controller-v3

--- src/main/java/hello/servlet/web/frontcontroller/v2/FrontControllerServletV2.java
+++ src/main/java/hello/servlet/web/frontcontroller/v2/FrontControllerServletV2.java
@@ -21,19 +21,19 @@ import java.util.Map;
 @WebServlet(name="frontControllerServletV2" , urlPatterns = "/front-controller/v2/*")
 public class FrontControllerServletV2 extends HttpServlet {
 
-    private Map<String, ControllerV2> controllerV1Map = new HashMap<>();
+    private Map<String, ControllerV2> controllerV2Map = new HashMap<>();
 
     public FrontControllerServletV2() {
-        controllerV1Map.put("/front-controller/v2/members/new-form", new MemberFormControllerV2());
-        controllerV1Map.put("/front-controller/v2/members/save", new MemberSaveControllerV2());
-        controllerV1Map.put("/front-controller/v2/members", new MemberListControllerV2());
+        controllerV2Map.put("/front-controller/v2/members/new-form", new MemberFormControllerV2());
+        controllerV2Map.put("/front-controller/v2/members/save", new MemberSaveControllerV2());
+        controllerV2Map.put("/front-controller/v2/members", new MemberListControllerV2());
     }
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String requestURI =  request.getRequestURI();
 
-        ControllerV2 controller = controllerV1Map.get(requestURI);
+        ControllerV2 controller = controllerV2Map.get(requestURI);
         if (controller == null){
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
 
